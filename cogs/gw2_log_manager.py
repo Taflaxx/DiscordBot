0ff47fe931272fef0701ed6dd4a3e0fcd9388f05
Taflaxx <13418652+Taflaxx@users.noreply.github.com>
2021-05-18 22:00:07 +0200

    can now add multiple logs at the same time

--- cogs/gw2_log_manager.py
+++ cogs/gw2_log_manager.py
@@ -8,7 +8,6 @@ import requests
 import json
 import re
 
-
 # Set up logging
 logger = logging.getLogger('sqlalchemy.engine')
 logger.setLevel(logging.DEBUG)
@@ -65,12 +64,19 @@ class LogManager(commands.Cog, name="log"):
             await ctx.send_help("log")
             print(f"Unknown subcommand \"{ctx.message.content}\" by {ctx.author}. Sent help page")
 
-    @log.command(name="add", aliases=["a"], help="Add a log to the database", usage="[log]")
-    async def add_log(self, ctx, log):
+    @log.command(name="add", aliases=["a"], help="Add logs to the database", usage="[log]")
+    async def add_logs(self, ctx, *, arg):
+        print(arg)
+        logs = re.findall("https:\/\/dps\.report\/[a-zA-Z\-0-9\_]+", arg)
+        print(f"Found {len(logs)} Logs")
+        for log in logs:
+            self.add_log(log)
+        db.commit()
+
+    def add_log(self, log):
         # Check if log already exists in the database
         if db.query(Log).filter_by(link=log).first():
-            await ctx.send("**`ERROR:`** Log already exists in the database")
-            print("Log is already in Database. Skipping...")
+            print(f"{log} | Already in Database")
             return
 
         # Get json data
@@ -79,17 +85,17 @@ class LogManager(commands.Cog, name="log"):
 
         # Check if boss was killed
         if not data["success"]:
-            await ctx.send("**`ERROR:`** Boss was not killed")
+            print(f"{log} | Boss was not killed")
             return
 
         # Create log in DB
         log_db = Log(link=log, fight_name=data["fightName"])
+        print(f"{log} | {data['fightName']}:")
 
         # Parse json data for each player
         for player in data["players"]:
             # Check if the player is an actual player and not a NPC
             if re.match("^[a-zA-Z]+\.(\d{4})$", player["account"]):
-                print(f"{log} | {data['fightName']} | {player['account']}")
                 player_db = Player(account=player["account"])
                 player_db.character_name = player["name"]
                 player_db.profession = player["profession"]
@@ -97,19 +103,21 @@ class LogManager(commands.Cog, name="log"):
                 player_db.damage_taken = player["defenses"][0]["damageTaken"]
                 log_db.players.append(player_db)
                 db.add(player_db)
+                print(f"{player_db.account} | {player_db.character_name} | {player_db.profession}")
             else:
-                print(f"{player['account']} is not a player. Skipping...")
+                print(f"{player['account']} | Not a player")
         db.add(log_db)
-        db.commit()
-        print("DB commit")
+        print("\n")
 
     @log.command(name="filter")
     async def filter_log(self, ctx, *args):
         result = db.query(Player).join(Log)
+
+        # Parsing arguments
         count = 0
         for arg in args:
             if arg == "-a":
-                print(args[count+1])
+                print(args[count + 1])
                 result = result.filter(Player.account == args[count + 1])
             elif arg == "-c":
                 result = result.filter(Player.character_name == args[count + 1])
@@ -117,15 +125,13 @@ class LogManager(commands.Cog, name="log"):
                 result = result.filter(Log.fight_name == args[count + 1])
             count += 1
         result = result.order_by(Player.dps_all.desc())
-        print(result)
+
+        # Create Embed
+        # Limited to top 9 logs
         embed = Embed(title="Logs")
-        for row in result:
+        for row in result[:9]:
             embed.add_field(name=f"{row.log.fight_name}", value=f"[Link]({row.log.link})\n DPS: {row.dps_all}")
         await ctx.send(embed=embed)
-
-    @log.command(name="sql")
-    async def sql_log(self, ctx, *text):
-        db.execute(" ".join(text))
 
 
 def setup(bot):
