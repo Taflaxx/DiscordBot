896f8cfbefb6c6f6238880a67b42929382bea321
Taflaxx <13418652+Taflaxx@users.noreply.github.com>
2022-08-09 21:55:24 +0200

    updated migration skript

--- scripts/migration_emboldened.py
+++ scripts/migration_emboldened.py
@@ -1,37 +1,48 @@
-import sqlite3
 import traceback
 import sys
-from cogs.logmanager.db import Log, add_log
 import datetime
+from cogs.logmanager.db import Log, add_log, db, engine
+from sqlalchemy import inspect
+import asyncio
 
-# Create new column
-con = sqlite3.connect("../cogs/logmanager/logmanager.db")
-cur = con.cursor()
-cur.execute("ALTER TABLE logs ADD COLUMN emboldened INTEGER DEFAULT 0")
+async def update_db():
+    # Delete all logs from db and then add them again to update values
+    date = datetime.datetime(year=2022, month=6, day=28)
+    query = db.query(Log).filter(Log.date_time >= date)
 
-from cogs.logmanager.db import db
+    count = query.count()
+    for idx, log in enumerate(query):
+        db.delete(log)
+        # "Ignore" errors here as very old logs with weird bugs/changes might get added that can just be ignored
+        try:
+            r = await add_log(log=log.link, guild_id=log.guild_id)
+        except Exception as error:
+            r = error
+            traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+        if r is not None:
+            print(r)
+        # Periodically update user on progress
+        if (idx + 1) % 10 == 0:
+            print(f"Progress: {idx + 1}/{count} logs")
+            db.commit()
 
-# Delete all logs from db and then add them again to update values
-date = datetime.datetime(year=2022, month=6, day=28)
-query = db.query(Log).filter(Log.date_time >= date)
-errors = 0
-error_str = ""
-count = query.count()
-for idx, log in enumerate(query):
-    db.delete(log)
-    # "Ignore" errors here as very old logs with weird bugs/changes might get added that can just be ignored
-    try:
-        r = await add_log(log=log.link, guild_id=log.guild_id)
-    except Exception as error:
-        r = error
-        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+if __name__ == "__main__":
+    # check if column exists and commit
+    insp = inspect(engine)
+    exists = False
+    for column in insp.get_columns("logs"):
+        if column["name"] == "emboldened":
+            print("Column already exists")
+            exists = True
+            break
+    if not exists:
+        print("Creating column...")
+        db.execute("ALTER TABLE logs ADD COLUMN emboldened INTEGER DEFAULT 0")
+        print("Finished creating column")
+    else:
+        print("Skipping column creation")
 
-    if r is not None:
-        print(r)
-        errors += 1
-        error_str += f"\n{r}"
-
-    # Periodically update user on progress
-    if (idx + 1) % 10 == 0:
-        print(f"Parsed {idx + 1}/{count} logs.{error_str}")
-        db.commit()
+    # Update logs in the db
+    print("Updating logs...")
+    asyncio.run(update_db())
+    print("Finished updating logs")
